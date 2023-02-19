f33b4f12694203552f34c3ebebb49b37cc31fdc9
amir-zarchinie <amzarchini@gmail.com>
2023-02-19 14:19:31 +0330

    create project for episode 4

---
+++ spring-security-ep4/src/main/java/com/example/springsecurityep4/SpringSecurityEp4Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityep4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityEp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityEp4Application.class, args);
+    }
+
+}
