823bac4e33953a8e4bba829e8d4a55fd818da57e
amir-zarchini <amzarchini@gmail.com>
2023-10-29 13:04:29 +0330

    add sql config add findUsername method

--- spring-security-fundamental2020/MultipleAuthenticationProvider/src/main/java/com/example/multipleauthenticationprovider/service/JpaUserDetailsService.java
+++ spring-security-fundamental2020/MultipleAuthenticationProvider/src/main/java/com/example/multipleauthenticationprovider/service/JpaUserDetailsService.java
@@ -3,22 +3,22 @@ package com.example.multipleauthenticationprovider.service;
 import com.example.multipleauthenticationprovider.entities.User;
 import com.example.multipleauthenticationprovider.repositories.UserRepository;
 import com.example.multipleauthenticationprovider.security.model.SecurityUser;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class JpaUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
         var o = userRepository.findUserByUsername(username);
-        User u = o.orElseThrow(() -> new UsernameNotFoundException(":("));
+        User u = o.orElseThrow(() -> new UsernameNotFoundException("username not found"));
         return new SecurityUser(u);
     }
 }
