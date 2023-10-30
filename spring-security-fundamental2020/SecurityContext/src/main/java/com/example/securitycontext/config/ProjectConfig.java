62c244c0e4f49527f4a9f664904d65cbb96b9586
amir-zarchini <amzarchini@gmail.com>
2023-10-30 10:30:52 +0330

    change Strategy method for async request for security context

--- spring-security-fundamental2020/SecurityContext/src/main/java/com/example/securitycontext/config/ProjectConfig.java
+++ spring-security-fundamental2020/SecurityContext/src/main/java/com/example/securitycontext/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import com.example.securitycontext.security.filters.UsernamePasswordAuthFilter;
 import com.example.securitycontext.security.providers.OtpAuthenticationProvider;
 import com.example.securitycontext.security.providers.TokenAuthProvider;
 import com.example.securitycontext.security.providers.UsernamePasswordAuthProvider;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -62,5 +64,13 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public InitializingBean initializingBean() {
+        return () -> {
+            SecurityContextHolder.setStrategyName(
+                    SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+        };
     }
 }
