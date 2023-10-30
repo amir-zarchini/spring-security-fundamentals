e7e58389dedbf4c270ae66dc8f70c5ebc3f68d70
amir-zarchini <amzarchini@gmail.com>
2023-10-30 09:11:43 +0330

    bug fix mysql config

--- spring-security-fundamental2020/MultipleAuthenticationProvider/src/main/java/com/example/multipleauthenticationprovider/entities/User.java
+++ spring-security-fundamental2020/MultipleAuthenticationProvider/src/main/java/com/example/multipleauthenticationprovider/entities/User.java
@@ -3,19 +3,17 @@ package com.example.multipleauthenticationprovider.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String username;
     private String password;
--- spring-security-fundamental2020/MultipleAuthenticationProvider/src/main/java/com/example/multipleauthenticationprovider/repositories/OtpRepository.java
+++ spring-security-fundamental2020/MultipleAuthenticationProvider/src/main/java/com/example/multipleauthenticationprovider/repositories/OtpRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface OtpRepository extends JpaRepository<Otp, Integer> {
+public interface OtpRepository extends JpaRepository<Otp, Long> {
 
     Optional<Otp> findOtpByUsername(String username);
 }
