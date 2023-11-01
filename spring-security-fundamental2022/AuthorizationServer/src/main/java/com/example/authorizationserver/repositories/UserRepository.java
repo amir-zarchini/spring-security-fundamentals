68a4a6f36d09288d189a5a5ff9f430703353a710
amir-zarchini <amzarchini@gmail.com>
2023-11-01 13:53:31 +0330

    add oauth2 projects: AuthorizationServer, Client, ResourceServer

---
+++ spring-security-fundamental2022/AuthorizationServer/src/main/java/com/example/authorizationserver/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.authorizationserver.repositories;
+
+import com.example.authorizationserver.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  @Query("SELECT u FROM User u WHERE u.username = :username")
+  Optional<User> findByUsername(String username);
+
+}
